ee27e4fcd9571fd531315b49c7ebca99cb87c122
Tech57625 <tech57625@gmail.com>
2025-12-12 07:20:32 +0000

    conversion of datatype

---
+++ javascript/conversionoperation.js
@@ -0,0 +1,28 @@
+/*let age = "20"
+
+console.log(typeof age);
+console.log(typeof (age));
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber);*/
+
+
+// "20" => 20
+// "20abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Raghav" => true
+
+let someNumber = 20
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);

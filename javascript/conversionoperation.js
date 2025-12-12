3de8a98f100afdc95309074b24fa5fd6cdf15bfc
Tech57625 <tech57625@gmail.com>
2025-12-12 07:47:39 +0000

    conversionoperation done

--- javascript/conversionoperation.js
+++ javascript/conversionoperation.js
@@ -26,3 +26,23 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ********************* Operation *************************
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Raghav"
+
+let str3 = str1 + str2
+
+//console.log(str3);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+

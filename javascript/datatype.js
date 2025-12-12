3e8dc83f31fdc2ff7772c25299cac88605641ed5
Tech57625 <tech57625@gmail.com>
2025-12-12 06:54:22 +0000

    datatype

---
+++ javascript/datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+let name = "Raghav"
+let age = 18
+let isLoggedIn = false 
+let state = null
+
+
+//console.table([age, isLoggedIn, state])
+
+
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+
+
+//object 
+
+console.log(typeof "Raghav");

bc58887ec4f9095abf80daae89499591b769d68f
Tech57625 <tech57625@gmail.com>
2025-12-13 08:04:23 +0000

    maths

---
+++ javascript/math.js
@@ -0,0 +1,31 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(2,3,4,5,6,7));
+console.log(Math.max(4,5,6,7,8));
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1)
+console.log((Math.floor(Math.random()*10) + 1))
+
+/////////////////////////////////////////////
+
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random() * (max -min)) + min);
+
+
+
+
+
+
+
+
+
+

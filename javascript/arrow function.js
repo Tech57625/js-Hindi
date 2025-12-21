e8038c8f00e9e4ddef59c2307088af60ff039cfb
Tech57625 <tech57625@gmail.com>
2025-12-21 09:48:05 +0000

    container config done

---
+++ javascript/arrow function.js
@@ -0,0 +1,15 @@
+const user = {
+    username: "raghav",
+    price: 999,
+
+    welcomeMessage: function () {
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+        
+    }
+}
+
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()
---
+++ javascript/arrow function1.js
@@ -0,0 +1,8 @@
+// arrow function
+
+const addTwo = (num1, num2) => (num1 + num2)
+
+console.log(addTwo(3,4));
+
+
+
